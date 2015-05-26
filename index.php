d2c43641150315511e02bcd673d4c130d81cd84f
inaLitso <inalitso@gmail.com>
2015-05-26 19:48:16 +0300

    first comment

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
  *
  * @package Piwik
  */
-
+/** tsekarei an uparxei to 'piwik_document_root' uparxei. */
 if (!defined('PIWIK_DOCUMENT_ROOT')) {
-    define('PIWIK_DOCUMENT_ROOT', dirname(__FILE__) == '/' ? '' : dirname(__FILE__));
+    define('PIWIK_DOCUMENT_ROOT', dirname(__FILE__) == '/' ? '' : dirname(__FILE__)); /** dirname Returns parent directory's path */
 }
 if (file_exists(PIWIK_DOCUMENT_ROOT . '/bootstrap.php')) {
     require_once PIWIK_DOCUMENT_ROOT . '/bootstrap.php';
@@ -24,4 +24,4 @@ if (!defined('PIWIK_PRINT_ERROR_BACKTRACE')) {
     define('PIWIK_PRINT_ERROR_BACKTRACE', false);
 }
 
-require_once PIWIK_INCLUDE_PATH . '/core/dispatch.php';
+require_once PIWIK_INCLUDE_PATH . '/core/dispatch.php'; /** kalei to dispatch.php opou sou bgazei minima lathous */
